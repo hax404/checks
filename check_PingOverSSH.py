e78ff6641f1bf5ecce7c4215cad4a50cf6f07609
Georg Haas <hax404foogit@hax404.de>
2016-11-27 16:44:02 +0100

    first working prototype for openwrt

--- check_PingOverSSH.py
+++ check_PingOverSSH.py
@@ -2,18 +2,19 @@
 
 import argparse
 import imp
+import paramiko
 
 def parseArgs():
 	parser = argparse.ArgumentParser(description="let external machine check ping")
 	parser.add_argument("--host", dest="host", required=True, help="Host that executes ping")
 	parser.add_argument("--username", dest="username", help="SSH username")
 	parser.add_argument("--password", dest="password", help="SSH password")
-	parser.add_argument("--os", choices=["openwrt","mikrotik"], default="openwrt", help="OS of the machine that executes ping. Default=openwrt")
+	parser.add_argument("--os", choices=["openwrt","mikrotik"], default="openwrt", required=True, help="OS of the machine that executes ping. Default=openwrt")
 	parser.add_argument("--ping-command", dest="ping_command", help="Ping Command")
 	parser.add_argument("--interface", dest="interface", help="the interface name from where the ping is sent")
 	parser.add_argument("--ttl", dest="ttl", help="the time to live that is passed to the ping command")
 	parser.add_argument("--count", dest="count", default="10", help="the number how many pings to sent for the statistic")
-	parser.add_argument("--dest", dest="destination", help="destination of ping")
+	parser.add_argument("--dest", dest="destination", required=True, help="destination of ping")
 	return parser.parse_args()
 
 def checkParamiko():
@@ -24,12 +25,28 @@ def checkParamiko():
 		print ("paramiko is not installed. You need the package paramiko.")
 		exit()
 
-def pingOpenWRT():
+def pingOpenWRT(args):
+	pingcommandlist = []
+	pingcommandlist.append("ping")
+	if(args.interface):
+		pingcommandlist.append("-I")
+		pingcommandlist.append(args.interface)
+	if(args.ttl):
+		pingcommandlist.append("-t")
+		pingcommandlist.append(args.ttl)
+	pingcommandlist.append("-c")
+	pingcommandlist.append(args.count)
+	pingcommandlist.append("-q")
+	pingcommandlist.append(args.destination)
+	pingcommand = " ".join(pingcommandlist)
+	print(pingcommand)
+
 	client = paramiko.SSHClient()
 	client.load_system_host_keys()
-	client.connect('kufuck.kkb.hax404.de',username='root',password='foobar')
-	stdin, stdout, stderr = client.exec_command('ping -I dn42_chero -t 0 -c 10 -q 172.22.251.150')
+	client.connect(args.host,username=args.username,password=args.password)
+	stdin, stdout, stderr = client.exec_command(pingcommand)
 	output = stdout.readlines()
+	print(output)
 	pkt_tx = output[3].split()[0]
 	pkt_rx = output[3].split()[3]
 	pkt_loss = output[3].split()[6]
@@ -49,6 +66,12 @@ def main():
 	print (parseArgs())
 	checkParamiko()
 	import paramiko
+	args=parseArgs()
+	print(args.os)
+	if(args.os == "openwrt"):
+		pingOpenWRT(args)
+
+
 
 
 if __name__ == "__main__":
